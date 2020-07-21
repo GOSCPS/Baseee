a27fc2c179dccbae5f51fb03c50276ce5f981c7e
chhd <sudo.free@qq.com>
2020-07-21 13:39:10 +0800

    update log

---
+++ src/log/log.hpp
@@ -0,0 +1,71 @@
+#ifndef _BASEEE_LOH_HPP_
+#define _BASEEE_LOH_HPP_
+/*Baseee::log
+ *log moudle
+ *MIT License
+ *coder:chhd 
+ *time:2020-7-21
+ *
+ *history:carry out
+ *
+ * 
+ */
+#include <string>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+namespace baseee{
+    namespace log{
+        class logger{
+            public:
+            //name:program name
+            //format:set log format
+            //out_format:set log out
+            logger(std::string name,std::string format,std::string out_format);
+
+            //print log for four level
+            void print_info(std::string log);
+            void print_important(std::string log);
+            void print_error(std::string log);
+            void print_fatal(std::string log);
+
+            //protected:
+            std::string name;
+            std::string log_format;
+            std::string out_format;
+
+            std::mutex mtx;
+
+            //std::vector<std::string> log_list;
+
+            std::fstream out_file;
+            std::string Lowest_level="error";
+            std::string file_name;
+
+
+            //format to string
+            std::string get_format(std::string level);
+            //set out format
+            void set_log();
+            //print log to file or clog
+            void out_log(std::string level,std::string log);
+        };
+    }
+}
+
+baseee::log::logger::logger(std::string name,std::string format,std::string out_format){
+    this->name = name;
+    this->log_format = format;
+    this->out_format = out_format;
+    set_log();
+    return;
+}
+
+#include "log.cpp"
+#include "out.cpp"
+
+
+#endif
